fc8bef429b9d8e97dae51c38dc2f99b04dff7622
BRODRIGUEZ <brayan.rodriguez@amadeus.com>
2025-11-22 13:48:28 -0500

    Change Swagger route to /swagger

--- EcommerceAPI.Tests/obj/Debug/net8.0/EcommerceAPI.Tests.AssemblyInfo.cs
+++ EcommerceAPI.Tests/obj/Debug/net8.0/EcommerceAPI.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceAPI.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f8dfef8f5a3b6df70017b273adad54bed7bb5f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+724700b0c1181f787f7287bb346cf3ca9843626a")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceAPI.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceAPI.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- EcommerceAPI/obj/Debug/net8.0/EcommerceAPI.AssemblyInfo.cs
+++ EcommerceAPI/obj/Debug/net8.0/EcommerceAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dbaa6788ac8f0d78c4503e94b1822c89b854a71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+724700b0c1181f787f7287bb346cf3ca9843626a")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
